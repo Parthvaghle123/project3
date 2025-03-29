559ffacbf6b0b2dbfd1f052478b917d9b4319707
Parthvaghle123 <vaghelaparth2005@gmail.com>
2025-03-29 23:24:31 +0530

    index3.html

--- validation2.js
+++ validation2.js
@@ -1,37 +1,88 @@
 $(document).ready(function() {
-    $('#show-payment-form').on('click', function() {
-        $('#payment-form').show();
-        $(this).hide();
+    $('#pay-now-btn').click(function() {
+        if (validateForm()) { 
+            $(this).html('<i class="fa fa-spinner fa-spin"></i>'); 
+            $(this).prop('disabled', true); 
+            $('#credit-card-form').submit();
+        }
     });
 
-    $('#credit-card-form').on('submit', function(e) {
+    $('#cvv').focus(function() {
+        $('#pay-now-btn').html('<i class="fa fa-credit-card"></i>'); 
+    });
+
+    $('#cvv').blur(function() {
+        $('#pay-now-btn').html('<i class="fa fa-credit-card"></i> Pay Now');
+    });
+
+    $('#credit-card-form').submit(function(e) {
         e.preventDefault();
-        $('#payment-form').hide();
-        $('#thank-you-message').show();
+
+        setTimeout(function() {
+            alert('Payment Successful!');
+            $('#exampleModal').modal('hide');
+
+            $('#pay-now-btn').html('<i class="fa fa-credit-card"></i> Pay Now');
+            $('#pay-now-btn').prop('disabled', false);
+        }, 2000);
     });
 
-    // Cardholder Name - Allow only letters and spaces
-    $('#cardholder_name').on('keypress', function(event) {
+    $('#exampleModal').on('hidden.bs.modal', function () {
+        $('#credit-card-form')[0].reset(); 
+    });
+
+    
+    $('#cardholder_name').keypress(function(event) {
         if (!/^[a-zA-Z\s]+$/.test(event.key)) {
             event.preventDefault();
         }
     });
 
-    // Card Number Formatting (Only numbers & Add Spaces)
+
     $('#card_number').on('input', function() {
-        let value = this.value.replace(/\D/g, '').substring(0, 16);
-        this.value = value.replace(/(.{4})/g, '$1 ').trim();
+        let cardNumber = this.value.replace(/\D/g, '').substring(0, 16);
+        this.value = cardNumber.replace(/(.{4})/g, '$1 ').trim();
     });
 
-    // Expiry Date Formatting (MM/YY)
+
     $('#expiry_date').on('input', function() {
         let value = this.value.replace(/\D/g, '');
-        if (value.length > 2) {
-            value = value.substring(0, 2) + '/' + value.substring(2, 4);
-        }
+        if (value.length > 2) value = value.substring(0, 2) + '/' + value.substring(2, 4);
         this.value = value;
-        if (value.length === 5) {
-            $('#cvv').focus();
-        }
     });
+
+
+    $('#cvv').on('input', function() {
+        this.value = this.value.replace(/\D/g, '').substring(0, 4);
+    });
+
+
+    function validateForm() {
+        let name = $('#cardholder_name').val().trim();
+        let cardNumber = $('#card_number').val().replace(/\s/g, '');
+        let expiryDate = $('#expiry_date').val();
+        let cvv = $('#cvv').val();
+
+        if (name.length < 3) {
+            alert('Cardholder Name should be at least 3 characters.');
+            return false;
+        }
+
+        if (cardNumber.length !== 16 || isNaN(cardNumber)) {
+            alert('Please enter a valid 16-digit Card Number.');
+            return false;
+        }
+
+        if (!/^(0[1-9]|1[0-2])\/\d{2}$/.test(expiryDate)) {
+            alert('Please enter a valid Expiry Date (MM/YY).');
+            return false;
+        }
+
+        if (cvv.length < 3 || cvv.length > 4 || isNaN(cvv)) {
+            alert('Please enter a valid CVV (3 or 4 digits).');
+            return false;
+        }
+
+        return true;
+    }
 });
